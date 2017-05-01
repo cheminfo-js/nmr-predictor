c3206c97007e1b4a2e6cf8a715df398cb21b5725
andcastillo <andcastillo@gmail.com>
2017-05-01 14:47:17 -0500

    change group function j coupling could not be always merged

--- test/testDisubstitutedAromaticRing.js
+++ test/testDisubstitutedAromaticRing.js
@@ -8,13 +8,13 @@ describe('Paradisubstitute prediction', function () {
     it('Expanded', async function () {
         this.timeout(10000);
         const prediction = await predictor.spinus(smile, {group: false});
-        console.log(JSON.stringify(prediction));
+        //console.log(JSON.stringify(prediction));
         prediction.length.should.equal(4);
     });
-    it.only('Grouped', async function () {
+    it('Grouped', async function () {
         this.timeout(10000);
         const prediction = await predictor.spinus(smile, {group: true});
-        console.log(JSON.stringify(prediction));
+        //console.log(JSON.stringify(prediction));
         prediction[0].j.length.should.eql(2);
         prediction.length.should.equal(2);
     });
